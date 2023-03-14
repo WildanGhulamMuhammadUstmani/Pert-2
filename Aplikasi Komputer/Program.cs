dbecbf664771c46f48d6d73de7c035f9567d9c9f
emailwildanzen0@gmail.com <arif.kurniawan@students.amikom.ac.id>
2023-03-14 14:57:37 +0700

    commit ke-1

---
+++ Aplikasi Komputer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikasi_Komputer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
